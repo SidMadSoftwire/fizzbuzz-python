25ca3a671c3f97a579cbe95e6742e581af7befd8
Siddharth Madhusudan <siddharth.madhusudan@softwire.com>
2025-04-24 12:09:17 +0100

    Add "Fezz" for multiples of 13
    Before first "B" or at end of string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 def fizzbuzz():
-    for i in range(1,101):
+    for i in range(1,300):
         word = ""
         if i % 3 == 0:
             word += "Fizz"
@@ -10,6 +10,12 @@ def fizzbuzz():
             word += "Bang"
         if i % 11 == 0:
             word = "Bong"
+        if i % 13 == 0:
+            b_loc = word.find("B")
+            if b_loc != -1:
+                word = f"{word[:b_loc]}Fezz{word[b_loc:]}"
+            else:
+                word += "Fezz"
         if word == "":
             print(i)
         else:
