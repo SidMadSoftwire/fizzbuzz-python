dbaeea0e9088830632f51139435a8a2d1f733688
Siddharth Madhusudan <siddharth.madhusudan@softwire.com>
2025-04-24 12:31:44 +0100

    Reverse order for multiples of 17

--- main.py
+++ main.py
@@ -2,20 +2,31 @@
 def fizzbuzz():
     for i in range(1,300):
         word = ""
+
         if i % 3 == 0:
             word += "Fizz"
+
         if i % 5 == 0:
             word += "Buzz"
+
         if i % 7 == 0:
             word += "Bang"
+
         if i % 11 == 0:
             word = "Bong"
+
         if i % 13 == 0:
             b_loc = word.find("B")
             if b_loc != -1:
                 word = f"{word[:b_loc]}Fezz{word[b_loc:]}"
             else:
                 word += "Fezz"
+
+        if i % 17 == 0:
+            if len(word) >= 8:
+                parts = [word[i:i+4] for i in range(0, len(word), 4)]
+                word = "".join(reversed(parts))
+
         if word == "":
             print(i)
         else:
