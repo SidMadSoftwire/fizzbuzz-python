18d47c8be909d6a277ea74709eec0a67af66a16a
Siddharth Madhusudan <siddharth.madhusudan@softwire.com>
2025-04-24 09:58:41 +0100

    Implement basic Fizzbuzz
    Multiples of 3 and 5 taken into account

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def fizzbuzz():
+    for i in range(1,101):
+        word = ""
+        if i % 3 == 0:
+            word += "Fizz"
+        if i % 5 == 0:
+            word += "Buzz"
+        if word == "":
+            print(i)
+        else:
+            print(word)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    fizzbuzz()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
